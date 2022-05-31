ead83c74330aa028339cb36f7041338a64e1e7e7
KuntaWasonga <wkunta@rocketmail.com>
2022-05-31 16:00:07 -0700

    Hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Prints the last part of quote to the standard error
+ * Return: 1 if success.
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n",58);
+	return (1);
+}
