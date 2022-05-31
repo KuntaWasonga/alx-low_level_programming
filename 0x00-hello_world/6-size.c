72c0e220006a8f59a84005a4a032633067c93e0e
KuntaWasonga <wkunta@rocketmail.com>
2022-05-31 15:39:21 -0700

    Hello_world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,19 +3,14 @@
  *  * main - Entry point
  *   *
  *    * Return: Always 0 (Success)
- * 
+ *
  */
 int main(void)
 {
-	char charType;
-	int intType;
-	long int LongIntType;
-	long long int LongLongIntType;
-	float floatType;
-	printf("Size of char: %d byte(s)\n", sizeof(charType));
-	printf("Size of int: %d byte(s)\n", sizeof(intType));
-	printf("Size of long int: %d byte(s)\n", sizeof(LongIntType));
-	printf("Size of long long int: %d byte(s)\n", sizeof(LongLongIntType));
-	printf("Size of float: %d byte(s)\n", sizeof(floatType));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
-}                        
+}
