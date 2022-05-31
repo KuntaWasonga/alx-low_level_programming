f5be0aee93b45c710e7cc65ea98fa0a4b6fd01d5
KuntaWasonga <wkunta@rocketmail.com>
2022-05-31 16:15:20 -0700

    Hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,8 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	str1="and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+	
+	write(2, srt1\n, 58);
 	return (1);
 }
