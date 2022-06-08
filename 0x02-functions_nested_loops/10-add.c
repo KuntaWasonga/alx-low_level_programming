50dce94e7df664e12ab309a6d0e0a2a5f9bb07db
KuntaWasonga <wkunta@rocketmail.com>
2022-06-07 18:36:51 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: first int to be added
+ * @b: second int to be added
+ * Return: ressult of addition
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
