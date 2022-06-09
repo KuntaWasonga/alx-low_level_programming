2124139b7afd1f5723b970fd81add6d8adf92e87
KuntaWasonga <wkunta@rocketmail.com>
2022-06-09 13:21:14 +0300

    More nested loops functions

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,10 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
-int _mul(int a, int b);
+int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -8,3 +12,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+
+#endif
