6e222af73cc4b4be32a4e3c20bf5f628862975ac
KuntaWasonga <wkunta@rocketmail.com>
2022-06-13 04:05:00 -0700

    Pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,6 +4,7 @@
  * reset_to_98 - takes a pointer to an int and updates
  * the value it points to 98.
  * @n: parameter
+ * Return: the value of the *n variable
  */
 
 void reset_to_98(int *n)
@@ -13,4 +14,5 @@ void reset_to_98(int *n)
 	n = &m;
 
 	m = 98;
+	return (*n);
 }
