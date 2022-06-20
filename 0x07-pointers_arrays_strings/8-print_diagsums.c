99033d6586101dae7392db607b83f089bbf0a67c
KuntaWasonga <wkunta@rocketmail.com>
2022-06-20 13:52:43 -0700

    Second commit

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -18,10 +18,10 @@ void print_diagsums(int *a, int size)
 	sum1 = 0;
 
 	for (i = 0; i < size; i++)
-		{
-			sum += a[(size * i) + i];
-			sum1 += a[(size * (i + 1)) - (i + 1)];
-		}
+	{
+		sum += a[(size * i) + i];
+		sum1 += a[(size * (i + 1)) - (i + 1)];
+	}
 
-	printf("%d, %d\n", sum, sum1);	
+	printf("%d, %d\n", sum, sum1);
 }
