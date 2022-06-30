58a90c8d4b44f21ed35677997d070ca129448168
KuntaWasonga <wkunta@rocketmail.com>
2022-06-30 14:25:29 -0700

    Mandatory tasks

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: size of memory to be allocated
+ *
+ * Return: returns a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *s;
+
+	s = malloc(b);
+
+	if (s == NULL)
+	{
+		exit(98);
+	}
+
+	return (s);
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * array_range - a function that creates an array of integers
+ * @min: intended minimum number
+ * @max: intended maximum number
+ *
+ * Return: returns the pointer to the newly created array
+ */
+
+int *array_range(int min, int max)
+{
+	int *s, n, i;
+
+	n = max - min + 1;
+
+	s = (int *)malloc(n * sizeof(int));
+
+	if (s == NULL || min > max)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; min <= max; i++)
+	{
+		s[i] = min++;
+	}
+
+	return (s);
+}
