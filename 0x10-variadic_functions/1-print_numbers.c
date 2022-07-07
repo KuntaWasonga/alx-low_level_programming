511996c6a21faedee92f98122d29de78d9063636
KuntaWasonga <wkunta@rocketmail.com>
2022-07-07 15:55:07 -0700

    Mandatory tasks

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,27 +10,22 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list ap;
+	va_list valist;
 	unsigned int i;
 
-	va_start(ap, n);
-
 	if (separator == NULL)
-	{
 		return;
-	}
-	else
-	{
-		for (i = 0; i < n; i++)
-		{
-			printf("%d", va_arg(ap, int));
 
-			if (i != n - 1)
-			{
-				printf("%c ", *separator);
-			}
-		}
-		printf("\n");
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(valist, const unsigned int));
+
+		if (i != (n - 1))
+			printf("%s", separator);
 	}
-	va_end(ap);
+	printf("\n");
+
+	va_end(valist);
 }
