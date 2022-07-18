4afd9cda3a4bb47c142ff6e5ecdaa21d74b213bd
KuntaWasonga <wkunta@rocketmail.com>
2022-07-18 09:32:21 -0700

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,13 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all elemtns of a list.
+ *
+ * @h: node
+ * Return: returns the number of nodes.
+ */
+
 size_t print_listint(const listint_t *h)
 {
-	    listint_t *ptr = h;
-	        int count = 0;
-		    
-		    while (ptr != NULL)
-			        {
-					     printf("%d\n", ptr->n);
-					          ptr=ptr->next;
-						       count++;
-						           }
-		        return (count);
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		_putchar(h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
