b17495b2048b73d5365095e355f93fc1be54638f
KuntaWasonga <wkunta@rocketmail.com>
2022-07-21 16:32:39 -0700

    Mandatory tasks

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,16 +1,22 @@
 #include "main.h"
 
+/**
+ * get_endianness - function that checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+
 int get_endianness(void)
 {
-  unsigned int x = 0x76543210;
-  char *c = (char*) &x;
- 
-  if (*c == 0x10)
-  {
-    return 1;
-  }
-  else
-  {
-     return 0;
-  }
+	unsigned int x = 0x76543210;
+	char *c = (char *) &x;
+
+	if (*c == 0x10)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
