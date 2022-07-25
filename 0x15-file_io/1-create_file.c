105d8db83f2fe9a461cefc98c5a1350494bcc0ce
KuntaWasonga <wkunta@rocketmail.com>
2022-07-25 12:57:53 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,41 +1,33 @@
 #include "main.h"
-#include <errno.h>
 
 /**
- * create_file - creates a file
- * @filename: is the name of the file
- * @text_content: a string content to be writen
+ * create_file - function that creates a file
  *
- * Return: 1 - on success, (-1) on failure
+ * @filename: pointer to the name of the file to create
+ * @text_content: pointer to NULL terminated string to be written to file
+ *
+ * Return: 1 on success and -1 on failure
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i;
+	int op, wr, len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		text_content = "";
 
-	fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0600);
-	if (fd < 0)
+	if (text_content != NULL)
 	{
-		if (errno == EEXIST)
-		{
-			fd = open(filename, O_WRONLY | O_TRUNC);
-			if (fd == -1)
-				return (-1);
-		}
-		else	
-			return (-1);
-	}
-	for (i = 0; text_content[i] != '\0'; i++)
-	{
-		if (write(fd, &text_content[i], 1) == -1);
-			return (-1);
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	close(fd);
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wr = write(op, text_content, len);
+
+	if (op == -1 || wr == -1)
+		return (-1);
+
+	close(op);
 	return (1);
 }
